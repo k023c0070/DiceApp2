6ee3d494d666668ecdb617e55234a654086f6041
k023c0070 <k023c0070@g.neec.ac.jp>
2024-12-19 14:12:07 +0900

    modified:   Game.java

--- Game.java
+++ Game.java
@@ -1,17 +1,28 @@
 public class Game {
-  Player p;
-  Dice d;
+  Player p1;
+  Player p2;
+  Dice d1;
+  Dice d2;
 
   Game(){
-    p = new Player();
-    d = new Dice();
+    p1 = new Player();
+    p2 = new Player();
+    d1 = new Dice();
+    d2 = new Dice();
   }
 
   void start(){
-    p.turn(d);
-    System.out.println(d.me);
+    p1.turn(d1);
+    p2.turn(d2);
+    judge();
   }
   void judge(){
-
+    if (d1.me > d2.me){
+      System.out.println("p1の勝ち");
+    }else if (d1.me == d2.me){
+      System.out.println("あいこ");
+    }else{
+      System.out.println("p2の勝ち");
+    }
   }
 }
